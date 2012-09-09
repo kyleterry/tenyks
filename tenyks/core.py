841a9a4b631f8a38557e07cb31ed71775cb90ea1
Kyle Terry <kyle@kyleterry.com>
2012-09-09 16:05:26 -0700

    Added reconnects to tenyks
    fixed handling of multiple connections... .get blocks so I added gevent
    connection_workers

--- tenyks/core.py
+++ tenyks/core.py
@@ -3,6 +3,7 @@ from os.path import join
 import re
 import sys
 import time
+import hashlib
 
 import gevent
 from gevent import socket
@@ -22,24 +23,43 @@ class Connection(object):
         self.config = config
         self.greenlets = []
         self.socket = socket.socket()
+        self.socket_connected = False
+        self.server_disconnect = False
+        self.user_disconnect = False
         self.input_queue = queue.Queue()
         self.input_buffer = ''
         self.output_queue = queue.Queue()
         self.output_buffer = ''
         log_directory = getattr(settings, 'LOG_DIRECTORY', '/tmp/tenyks')
-        self.log_file = join(log_directory.split('/'), 'irc-%s.log' % self.name)
+        self.log_file = join(log_directory, 'irc-%s.log' % self.name)
 
-    def connect(self):
+    def connect(self, reconnecting=False):
         while True:
             try:
+                if reconnecting:
+                    print 'Reconnecting to %s' % self.name
+                else:
+                    print 'Connecting to %s...' % self.name
                 self.socket.connect((self.config['host'], self.config['port']))
+                self.socket_connected = True
+                self.server_disconnect = False
+                print 'Successfully connected to %s' % self.name
                 break
             except socket.error as e:
                 print 'Could not connect to %s: Retrying' % self.name
+                time.sleep(5)
         self.spawn_send_and_recv_loops()
+
+    def reconnect(self):
+        for greenlet in self.greenlets:
+            greenlet.kill()
+        self.socket.close()
+        self.socket = socket.socket()
+        self.connect(reconnecting=True)
 
     def close(self):
         self.socket.close()
+        self.user_disconnect = True
 
     def spawn_send_and_recv_loops(self):
         self.greenlets.append(gevent.spawn(self.recv_loop))
@@ -48,6 +68,13 @@ class Connection(object):
     def recv_loop(self):
         while True:
             data = self.socket.recv(1024).decode('utf-8')
+            if not data:
+                print 'Disconnected'
+                self.socket_connected = False
+                self.server_disconnect = True
+                break
+            with open(self.log_file, 'a+') as log_file:
+                log_file.write('receiving: %s' % data)
             self.input_buffer += data
             while '\r\n' in self.input_buffer:
                 line, self.input_buffer = self.input_buffer.split('\r\n', 1)
@@ -56,12 +83,14 @@ class Connection(object):
     def send_loop(self):
         while True:
             line = self.output_queue.get()
-            print 'sending: %s' % line
             self.output_buffer += line.encode('utf-8', 'replace') + '\r\n'
             while self.output_buffer:
                 sent = self.socket.send(self.output_buffer)
                 self.output_buffer = self.output_buffer[sent:]
                 time.sleep(.5)
+
+    def needs_reconnect(self):
+        return not self.socket_connected and self.server_disconnect
 
 
 class IrcLine(object):
@@ -71,6 +100,8 @@ class IrcLine(object):
     irc_param_ref = re.compile(r'(?:^|(?<= ))(:.*|[^ ]+)').findall
 
     def __init__(self, connection, raw_line):
+        self.line_id = hashlib.sha256(
+                str(time.time()) + raw_line).hexdigest()[:10]
         self.raw_line = raw_line
         self.connection = connection
         if raw_line.startswith(":"):  # has a prefix
@@ -104,6 +135,18 @@ class IrcLine(object):
     def __repr__(self):
         return '<%s: %s>' % (self.nick_from, self.message)
 
+    def to_redis_line(self):
+        return redisline_factory({
+            'version': 1,
+            'type': 'privmsg',
+            'data': {
+                'nick_from': self.nick_from,
+                'message': self.message,
+                'irc_channel': self.channel,
+                'is_direct': self.direct,
+            }
+        })
+
 
 class RedisLineVersionMismatch(Exception):
     pass
@@ -122,22 +165,30 @@ class RedisLineV1(RedisLine):
     def __init__(self, raw_line):
         if type(raw_line) in (str, unicode,):
             raw_line = json.loads(raw_line)
-        if raw_line.version != self.exact_version:
+        if raw_line['version'] != self.exact_version:
             raise RedisLineVersionMismatch(
                 'Instances of this class must be created with version %d of the API' % 
                 (self.exact_version))
-        self.version = raw_line.version
-        self.type = raw_line.type
-        self.message = raw_line.data['message']
-        self.service_name = raw_line['service_name']
-        self.data = raw_line.data
+        self.version = raw_line['version']
+        self.type = raw_line['type']
+        self.message = raw_line['data']['message']
+        self.service_name = raw_line['service_name'] if 'service_name' \
+            in raw_line else None
+        self.data = raw_line['data']
 
     def to_publish(self):
-        return json.dumps()
+        return json.dumps({
+            'version': self.version,
+            'type': self.type,
+            'service_name': self.service_name,
+            'data': self.data,
+        })
 
 
-def redisline_factory(raw_line):
-    return RedisLineV1(raw_line)
+def redisline_factory(raw_line, version=1):
+    if version == 1:
+        return RedisLineV1(raw_line)
+    return None
 
 
 class Robot(object):
@@ -156,6 +207,9 @@ class Robot(object):
     """
 
     def __init__(self):
+        self.broadcast_queue = queue.Queue()
+        gevent.spawn(self.broadcast_loop)
+
         self.connections = {}
         self.bootstrap_connections()
 
@@ -190,7 +244,7 @@ class Robot(object):
         always returns None
         """
         message = message.replace('PING', 'PONG')
-        self.send(connection.name, self.make_pong(message))
+        self.send(connection.name, message)
 
     def send(self, connection, message):
         """
@@ -199,13 +253,27 @@ class Robot(object):
         message = message.strip()
         self.connections[connection].output_queue.put(message)
 
-    def broadcast(self, line):
+    def broadcast_loop(self):
         """
-        Broadcase a line from IRC to all the services
+        Pop things off the broadcast_queue and create jobs for them.
         """
-        pass
+        while True:
+            irc_line = self.broadcast_queue.get()
+            gevent.spawn(self.broadcast_worker, irc_line)
+
+    def broadcast_worker(self, irc_line):
+        """
+        This worker will broadcast a message to the service broadcast channel
+        """
+        r = redis.Redis(**settings.REDIS_CONNECTION)
+        broadcast_channel = getattr(settings, 'SERVICES_BROADCAST_CHANNEL',
+            'tenyks.services.broadcast')
+        r.publish(broadcast_channel, irc_line.to_redis_line().to_publish())
 
     def fetch_online_services(self):
+        pass
+
+    def fetch_offline_services(self):
         pass
 
     def fetch_all_services(self):
@@ -214,19 +282,40 @@ class Robot(object):
     def run(self):
         try:
             while True:
+                self.workers = []
                 for name, connection in self.connections.iteritems():
-                    raw_line = connection.input_queue.get()
-                    if raw_line.startswith('PING'):
-                        self.handle_irc_ping(connection, raw_line)
-                        continue
-                    irc_line = IrcLine(connection, raw_line)
-                    self.broadcast(irc_line)
+                    self.workers.append(gevent.spawn(self.connection_worker, connection))
+                gevent.joinall(self.workers)
+                break
         except KeyboardInterrupt:
+            print 'Shutting down: User disconnect'
+            for name, connection in self.connections.iteritems():
+                connection.close()
+        finally:
             for name, connection in self.connections.iteritems():
                 self.send(connection.name,
                         'QUIT :%s' % getattr(self, 'exit_message', ''))
                 connection.close()
             sys.exit('Bye.')
+
+    def connection_worker(self, connection):
+        while True:
+            if connection.user_disconnect:
+                break
+            if connection.needs_reconnect():
+                connection.reconnect()
+                self.set_nick_and_join(connection)
+            try:
+                raw_line = connection.input_queue.get(timeout=5)
+            except queue.Empty:
+                continue
+            if raw_line.startswith('PING'):
+                self.handle_irc_ping(connection, raw_line)
+                continue
+            else:
+                irc_line = IrcLine(connection, raw_line)
+                self.broadcast_queue.put(irc_line)
+        print 'Worker shutdown'
 
 
 if __name__ == '__main__':
