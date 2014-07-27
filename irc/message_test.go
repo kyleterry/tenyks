c30fa3fea3fa38426cf78e1d2de8f433abc203e8
Kyle Terry <kyle@kyleterry.com>
2014-07-26 23:08:55 -0700

    Added message tests

---
+++ irc/message_test.go
@@ -0,0 +1,21 @@
+package irc
+
+import(
+	"testing"
+)
+
+func TestParseMessage(t *testing.T) {
+	msg_string := ":kyle!~kyle@localhost PRIVMSG #tenyks :tenyks: messages are awesome"
+	msg := ParseMessage(msg_string)
+	if msg == nil {
+		t.Error("Expected", Message{}, "got", msg)
+	}
+
+	if msg.Command != "PRIVMSG" {
+		t.Error("Expected", Message{}, "got", msg)
+	}
+
+	if msg.Trail != "tenyks: messages are awesome" {
+		t.Error("Expected", "tenyks: messages are awesome", "got", msg)
+	}
+}
