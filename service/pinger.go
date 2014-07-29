06c497f8d24660cd15c53322a22a8e326bf339e6
Kyle Terry <kyle@kyleterry.com>
2014-07-29 00:50:02 -0700

    Ser

---
+++ service/pinger.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"time"
+	"encoding/json"
+)
+
+const (
+	ServiceOnline = true
+	ServiceOffline = false
+)
+
+func (self *Connection) PingServices() {
+	log.Debug("[service] Starting pinger")
+	for {
+		<-time.After(time.Second * 120)
+		log.Debug("[service] PINGing services")
+		msg := &Message{
+			Command: "PING",
+			Payload: "!tenyks",
+		}
+		jsonBytes, err := json.Marshal(msg)
+		if err != nil {
+			log.Error("Cannot marshal PING message")
+			continue
+		}
+		self.Out <- string(jsonBytes[:])
+
+		services := self.engine.ServiceRg.services
+		for _, service := range services {
+			service.LastPing = time.Now()
+		}
+	}
+}
+
+func (self *Connection) PongServiceHandler(msg *Message) {
+	meta := msg.Meta
+	self.engine.UpdateService(meta.Name, ServiceOnline)
+}
