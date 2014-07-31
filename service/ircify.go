490e137ee369a173fcfff473c5d2b413a562e627
Kyle Terry <kyle@kyleterry.com>
2014-07-31 01:13:52 -0700

    Removed useless duplication

--- service/ircify.go
+++ service/ircify.go
@@ -14,11 +14,9 @@ type Message struct {
 	Direct       bool   `json:"direct"`
 	Nick         string `json:"nick"`
 	Host         string `json:"host"`
-	FullMsg      string `json:"fullmsg"`
-	Full_message string `json:"full_message"` // Legacy for compat with py version
+	Full_message string `json:"full_message"`
 	User         string `json:"user"`
-	FromChannel  bool   `json:"fromchannel"`
-	From_channel bool   `json:"from_channel"` // Legacy for compat with py version
+	From_channel bool   `json:"from_channel"`
 	Connection   string `json:"connection"`
 	Payload      string `json:"payload"`
 	Meta         *Meta  `json:"meta"`
