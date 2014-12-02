2e70604bb3032c76174cca4364a5bb987d8c1921
Kyle Terry <kyle@kyleterry.com>
2014-12-02 03:15:15 -0800

    Progress on the connection manager

--- irc/manager.go
+++ irc/manager.go
@@ -1,24 +1,29 @@
 package irc
 
 import (
-
+	"time"
 )
 
 const (
 	RqNoResponse = iota
 	RqResponseRequired
-	RqJoin
-	RqPart
-	RqInfo
+	RtJoin
+	RtPart
+	RtInfo
+	RtNewConn
+	RtDisconnect
+	RtReconnect
 )
 
 type Request struct {
 	RequestType int
 	Requirement int
+	Payload interface{}
 }
 
 type ConnectionManager struct {
 	connections map[string]*Connection
+	cmCom chan chan Request
 }
 
 func NewConnectionManager() *ConnectionManager {
@@ -27,22 +32,48 @@ func NewConnectionManager() *ConnectionManager {
 	}
 }
 
-func (cm *ConnectionManager) Start(done chan bool) chan chan Event {
-	c := make(chan chan Event)
+func (cm *ConnectionManager) Start(done chan bool) chan chan Request {
+	c := make(chan chan Request)
+
 	// Event loop
 	go func() {
 		for {
 			select {
 			case com := <-c:
-				//handle
+				select {
+				case req := <-com:
+					if req.Requirement == RqNoResponse {
+						close(com)
+					}
+					switch req.RequestType {
+					case RtNewConn:
+						conn := req.Payload.(*Connection)
+						cm.connections[conn.Name] = conn
+					case RtDisconnect:
+						connName := req.Payload.(string)
+						if conn, ok := cm.connections[connName]; ok {
+							conn.Disconnect()
+						}
+					}
+				case <-done:
+				case <-time.After(time.Second * 5):
+				}
 			case <-done:
 				return
 			}
 		}
 	}()
+
 	return c
 }
 
-func (cm *ConnectionManager) HandleEvent(event Event) {
-
+func (cm *ConnectionManager) AddConnection(conn *Connection) {
+	r := Request{
+		RequestType: RtNewConn,
+		Requirement: RqNoResponse,
+	}
+	c := make(chan Request)
+	c <- r
+	cm.cmCom <- c
+	<-c
 }
